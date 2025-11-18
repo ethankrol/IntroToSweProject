d1035a8266d509385462314fba841d5ae69f2adf
okar0 <oskar.dolega@gmail.com>
2025-11-18 11:39:29 -0500

    Make .env directory relative with a  BASE_DIR

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,10 +1,17 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent  # points to backend/
 
 class Settings(BaseSettings):
     SECRET_KEY: str
-    ALGORITHM: str = 'HS256'
+    ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     MONGO_URL: str
-    model_config = SettingsConfigDict(env_file = 'C:\\Users\\AMW\\Documents\\IntroToSWE\\IntroToSweProject\\backend\\app\\.env')
+
+    model_config = SettingsConfigDict(
+        env_file=BASE_DIR / ".env",
+        env_file_encoding="utf-8",
+    )
 
 settings = Settings()
